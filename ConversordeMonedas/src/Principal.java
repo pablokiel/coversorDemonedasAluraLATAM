d5726173a245e3b5c4ae7f613794a8a83131b62b
Pablo Kiel Castellanos Vargas <31465703+pablokiel@users.noreply.github.com>
2025-05-19 20:10:39 -0600

    Update
    Se agregan sout en el menus del swicth y se agrega el HTTP client y request

--- ConversordeMonedas/src/Principal.java
+++ ConversordeMonedas/src/Principal.java
@@ -1,65 +1,120 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
 
-            Scanner introduce = new Scanner(System.in);
-            int opcion = 0;
+        Scanner introduce = new Scanner(System.in);
+        int opcion = 0;
+        double cantidad = 0;
 
-            while (opcion != 7) {
+        while (opcion != 7) {
 
-                System.out.println("***************************************");
-                System.out.println("Sea Bienvenido al conversor de monedas");
-                System.out.println("***************************************\n");
-                System.out.println("Por favor escoja la operaciòn que quiere realizar\nConvertir de: \n");
-                System.out.println("1) Dolar a Peso Argentino");
-                System.out.println("2) Peso Argentino a Dolar");
-                System.out.println("3) Dolar a Real Brasileño");
-                System.out.println("4) Real Brasileño a Dolar");
-                System.out.println("5) Dolar a Peso Columbiano");
-                System.out.println("6) Peso Columbiano a Dolar");
-                System.out.println("7) Salir\n");
-                System.out.println("**************************************\n");
-                System.out.println("Elija una opcion");
+            System.out.println("***************************************");
+            System.out.println("Sea Bienvenido al conversor de monedas");
+            System.out.println("***************************************\n");
+            System.out.println("Por favor escoja la operaciòn que quiere realizar\nConvertir de: \n");
+            System.out.println("1) Dolar a Peso Argentino");
+            System.out.println("2) Peso Argentino a Dolar");
+            System.out.println("3) Dolar a Real Brasileño");
+            System.out.println("4) Real Brasileño a Dolar");
+            System.out.println("5) Dolar a Peso Mexicano");
+            System.out.println("6) Peso Mexicano a Dolar");
+            System.out.println("7) Salir\n");
+            System.out.println("**************************************\n");
+            System.out.println("Elija una opcion");
 
-                try {
-                    opcion = introduce.nextInt();
-                    introduce.close();
+            try {
 
-                    switch (opcion) {
-                        case 1:
-                            System.out.println("Opcion 1");
-                            break;
-                        case 2:
-                            System.out.println("Opcion 2");
-                            break;
-                        case 3:
-                            System.out.println("Opcion 3");
-                            break;
-                        case 4:
-                            System.out.println("Opcion 4");
-                            break;
-                        case 5:
-                            System.out.println("Opcion 5");
-                            break;
-                        case 6:
-                            System.out.println("Opcion 6");
-                            break;
-                        case 7:
-                            System.out.println("Opcion 7");
-                            break;
 
-                        default:
-                            System.out.println("\n¡Opción no válida! Por favor, elige un número del 1 al 7.");
+                String direccion = "https://v6.exchangerate-api.com/v6/e2a19d36011125e680507af6/pair/";
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create(direccion))
+                        .build();
+//                   HttpResponse<String> response = client
+//                            .send(request, HttpResponse.BodyHandlers.ofString());
 
-                    }
-                }catch (Exception e) {
 
-                    System.out.println("\nError: Debes ingresar un número entero válido.\n");
-                    introduce.nextLine();
+                opcion = introduce.nextInt();
+
+
+                switch (opcion) {
+
+                    // siglas que se usan USD  MXN  ARS  BRL
+                    case 1:
+                        System.out.println("Introduce la cantidad de Dolares que quieres convertir a Pesos Argentinos");
+                        cantidad = introduce.nextDouble();
+
+
+                        direccion = direccion+"/USD/ARS/"+cantidad;
+
+                        System.out.println(cantidad+" USD son: "+" ARS");
+                        break;
+                    case 2:
+                        System.out.println("Introduce la cantidad de Pesos Argentinos que quieres convertir a Dolares");
+                        cantidad = introduce.nextDouble();
+
+
+                        direccion = direccion+"/ARS/USD/"+cantidad;
+
+                        System.out.println(cantidad+" ARS son: "+ " USD");
+                        break;
+                    case 3:
+                        System.out.println("Introduce la cantidad de Dolares que quieres convertir a Reales Brasileños");
+                        cantidad = introduce.nextDouble();
+
+
+                        direccion = direccion+"/USD/BRL/"+cantidad;
+
+                        System.out.println(cantidad+" USD son: "+" BRL");
+                        break;
+                    case 4:
+                        System.out.println("Introduce la cantidad de Reales Brasileños que quieres convertir a Dolares");
+                        cantidad = introduce.nextDouble();
+
+
+                        direccion = direccion+"/BRL/USD/"+cantidad;
+
+                        System.out.println(cantidad+" BRL son: "+ " USD");
+                        break;
+                    case 5:
+                        System.out.println("Introduce la cantidad de Dolares que quieres convertir a Pesos Mexicanos");
+                        cantidad = introduce.nextDouble();
+
+
+                        direccion = direccion+"/USD/MXN/"+cantidad;
+
+                        System.out.println(cantidad+" USD son: "+ " MXN");
+                        break;
+                    case 6:
+                        System.out.println("Introduce la cantidad de Pesos Mexicanos que quieres convertir a Dolares");
+                        cantidad = introduce.nextDouble();
+
+
+                        direccion = direccion+"/MXN/USD/"+cantidad;
+
+                        System.out.println(cantidad+" MXN son: "+ " USD");
+                        break;
+                    case 7:
+                        System.out.println("Hasta Luego");
+                        break;
+
+                    default:
+                        System.out.println("\n¡Opción no válida! Por favor, elige un número del 1 al 7.");
+
+                }
+
+            } catch (Exception e) {
+
+                System.out.println("\nError: Debes ingresar un número válido. Intenta de nuevo\n");
+                introduce.nextLine();
+
 
             }
-            System.out.println("El programa ha fallado");
         }
     }
 }
